b42d72d15a3a273eb3101c595171bc3ffca60cc1
shaps26 <shanshapeche@gmail.com>
2017-08-02 14:57:44 +0200

    architecture

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var exec = require('exec');
 
-app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/public'));    
 app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + 'public/index.html');
 });
 
 server.listen(8080, function(){
